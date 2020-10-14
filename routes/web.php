aa7b1340b890085defb1424c035dcfb7f37a69b7
Yohann Nizon <ynizon@gmail.com>
2020-10-14 21:05:00 +0200

    FIX : ip check

--- routes/web.php
+++ routes/web.php
@@ -16,33 +16,36 @@
 Route::get('/maintenance', 'MaintenanceController@index');
 Route::get('/maintenance/admin', 'MaintenanceController@admin');
 Route::post('/maintenance/admin', 'MaintenanceController@admin');
+Route::get('/busy', 'HomeController@busy');
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/download', 'HomeController@download');
-Route::post('/download', 'HomeController@download');
 
-Route::get('/mydownload/mp3', 'HomeController@download');
+if (config("app.ONLY_IP") == "" or in_array($_SERVER['REMOTE_ADDR'],config("app.ONLY_IP"))){
+	Route::get('/', 'HomeController@index');
+	Route::get('/sitemap.xml', 'HomeController@sitemap');
+	Route::get('/contact', 'HomeController@contact');
+	Route::get('/faq', 'HomeController@faq');
+	Route::get('/lastfm_login', 'HomeController@lastfm_login');
+	Route::post('/lastfm_login', 'HomeController@lastfm_login');
 
-Route::get('/', 'HomeController@index');
-Route::get('/sitemap.xml', 'HomeController@sitemap');
-Route::get('/contact', 'HomeController@contact');
-Route::get('/faq', 'HomeController@faq');
-Route::get('/lastfm_login', 'HomeController@lastfm_login');
-Route::post('/lastfm_login', 'HomeController@lastfm_login');
+	Route::get('/search', 'SearchController@index');
+	Route::get('/ajax/flip/{artist_name}', 'AjaxController@flip');
+	Route::get('/ajax/autocomplete', 'AjaxController@autocomplete');
+	Route::post('/ajax/keyword', 'AjaxController@keyword');
 
-Route::get('/search', 'SearchController@index');
-Route::get('/ajax/flip/{artist_name}', 'AjaxController@flip');
-Route::get('/ajax/autocomplete', 'AjaxController@autocomplete');
-Route::post('/ajax/keyword', 'AjaxController@keyword');
+	Route::get('/ajax/artist/{artist_name}', 'AjaxController@artist');
+	Route::get('/ajax/artist/{artist_name}/{album_name}', 'AjaxController@artist_album');
+	Route::get('/ajax/artist/{artist_name}/{album_name}/{title_name}', 'AjaxController@artist_album_title');
 
-Route::get('/ajax/artist/{artist_name}', 'AjaxController@artist');
-Route::get('/ajax/artist/{artist_name}/{album_name}', 'AjaxController@artist_album');
-Route::get('/ajax/artist/{artist_name}/{album_name}/{title_name}', 'AjaxController@artist_album_title');
-
-Route::get('/artist', 'SearchController@index');
-Route::get('/artist/{artist_name}', 'SearchController@artist');
-Route::get('/artist/{artist_name}/{album_name}', 'SearchController@artist_album');
-Route::get('/artist/{artist_name}/{album_name}/{title_name}', 'SearchController@artist_album_title');
-
+	Route::get('/artist', 'SearchController@index');
+	Route::get('/artist/{artist_name}', 'SearchController@artist');
+	Route::get('/artist/{artist_name}/{album_name}', 'SearchController@artist_album');
+	Route::get('/artist/{artist_name}/{album_name}/{title_name}', 'SearchController@artist_album_title');	
+}else{
+	if ($_SERVER['REQUEST_URI'] != "/busy"){
+		header("location: /busy");
+		exit();
+	}
+}
 //On filtre par permission
 Route::group(['middleware' => ['auth','permission:user-edit']], function () {	
 		Route::resource('users', 'UserController');
